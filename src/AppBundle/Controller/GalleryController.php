2a76edabb694d18f89ad30c9079d124056c2e50f
Frantiq90 <oscar.kosela@gmail.com>
2017-09-10 18:15:59 +0000

    pagination

--- src/AppBundle/Controller/GalleryController.php
+++ src/AppBundle/Controller/GalleryController.php
@@ -11,20 +11,29 @@ class GalleryController extends Controller
     /**
      * @Route("/gallery", name="gallery")
      */
-    public function indexAction()
+    public function indexAction(Request $request)
     {
+        $images = [
+            'daim-dans-la-foret.jpg',
+            'daim-dans-la-foret.jpg',
+            'daim-dans-la-foret.jpg',
+            'daim-dans-la-foret.jpg',
+            'daim-dans-la-foret.jpg',
+            'daim-dans-la-foret.jpg',
+            'daim-dans-la-foret.jpg',
+            'daim-dans-la-foret.jpg',
+            'daim-dans-la-foret.jpg'
+        ];
+
+        $paginator  = $this->get('knp_paginator');
+        $pagination = $paginator->paginate(
+            $images,
+            $request->query->getInt('cart', 1)/*page number*/,
+            4/*limit per page*/
+        );
+
         return $this->render('gallery/index.html.twig', [
-            'images' => [
-                'daim-dans-la-foret.jpg',
-                'daim-dans-la-foret.jpg',
-                'daim-dans-la-foret.jpg',
-                'daim-dans-la-foret.jpg',
-                'daim-dans-la-foret.jpg',
-                'daim-dans-la-foret.jpg',
-                'daim-dans-la-foret.jpg',
-                'daim-dans-la-foret.jpg',
-                'daim-dans-la-foret.jpg'
-            ]
+            'images' => $pagination
         ]);
     }
 }
