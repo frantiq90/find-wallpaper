4c23a213c2c5e1785032ee959b99996b389c29bb
Frantiq90 <oscar.kosela@gmail.com>
2017-09-11 11:49:33 +0000

    console styling for app:set-wallpapers command

--- src/AppBundle/Command/SetupWallpapersCommand.php
+++ src/AppBundle/Command/SetupWallpapersCommand.php
@@ -5,8 +5,10 @@ namespace AppBundle\Command;
 use AppBundle\Entity\Wallpaper;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Helper\Table;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
 
 class SetupWallpapersCommand extends Command
 {
@@ -33,22 +35,54 @@ class SetupWallpapersCommand extends Command
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
+        $io = new SymfonyStyle($input, $output);
+
         $wallpapers = glob($this->rootDir . '/../web/images/*.*');
 
+        $wallpaperCount = count($wallpapers);
+        $io->title('Importing Wallpapers...');
+        $io->progressStart($wallpaperCount);
+
+        $fileNames = [];
+
         foreach ($wallpapers as $wallpaper) {
+
+            [
+                'basename' => $filename,
+                'filename' => $slug
+            ] = pathinfo($wallpaper);
+
+            [
+                0 => $width,
+                1 => $height
+            ] = getimagesize($wallpaper);
+
             $wp = (new Wallpaper())
-                ->setFilename($wallpaper)
-                ->setSlug($wallpaper)
-                ->setWidth(1920)
-                ->setHeight(1080)
+                ->setFilename($filename)
+                ->setSlug($slug)
+                ->setWidth($width)
+                ->setHeight($height)
                 ;
 
             $this->em->persist($wp);
+
+            $io->progressAdvance();
+
+            $fileNames[] = [$filename];
         }
 
         $this->em->flush();
 
-        $output->writeln('Command result.');
+        $io->progressFinish();
+
+        $table = new Table($output);
+        $table
+            ->setHeaders(['Filename'])
+            ->setRows($fileNames)
+            ;
+        $table->render();
+
+        $io->success(sprintf('Success! %d wallpapers added to DB', $wallpaperCount));
     }
 
 }
